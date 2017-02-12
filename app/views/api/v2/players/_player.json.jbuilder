b06daf69a18f415f79fdf4828b2d6593e093c4b0
avandeloo <anthony.vandeloo@gmail.com>
2017-02-12 13:04:15 -0600

    added partial file to serve as template for json show and index pages

---
+++ app/views/api/v2/players/_player.json.jbuilder
@@ -0,0 +1,6 @@
+json.id player.id
+json.full_name "#{player.first_name} #{player.last_name}"
+json.position player.position
+json.birthday player.birthdate
+json.pro_team player.pro_team
+json.location player.location
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  get '/players' => 'players#index'
-  post '/players' => 'players#create'
-  get '/players/:id' => 'players#show'
-  patch '/players/:id' => 'players#update'
-  delete '/players/:id' => 'players#destroy'
+  namespace :api do
+    
+    namespace :v1 do
+      get '/players' => 'players#index'
+      post '/players' => 'players#create'
+      get '/players/:id' => 'players#show'
+      patch '/players/:id' => 'players#update'
+      delete '/players/:id' => 'players#destroy'
+    end
 
+    namespace :v2 do
+      get '/players' => 'players#index'
+      post '/players' => 'players#create'
+      get '/players/:id' => 'players#show'
+      patch '/players/:id' => 'players#update'
+      delete '/players/:id' => 'players#destroy'
+    end
+
+  end
 
 
 end
