f51903f39c6538d9dcbfbe41f8b6e22b8bfb24d5
avandeloo <anthony.vandeloo@gmail.com>
2017-02-18 19:17:27 -0600

    refactored code to incorporate api key access for a specified user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210013431) do
+ActiveRecord::Schema.define(version: 20170214024820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170210013431) do
     t.decimal  "salary"
     t.string   "location"
     t.text     "quote"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
